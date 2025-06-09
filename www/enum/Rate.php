04009630202452d43a9822cf2c8898c60bf17167
parkerru <parker.volkov@yandex.ru>
2025-06-09 03:18:14 +0300

    task 1 - 10

---
+++ www/enum/Rate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace enum;
+
+class Rate
+{
+    // рубли
+    const RUB = [
+        'name' => 'RUB',
+        'index' => 1,
+    ];
+    // евро
+    const EUR = [
+        'name' => 'EUR',
+        'index' => 2,
+    ];
+    // доллары
+    const USD = [
+        'name' => 'USD',
+        'index' => 3,
+    ];
+}
